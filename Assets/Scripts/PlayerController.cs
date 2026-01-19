a90017b99729f9f3417391455c69a0ccb7ed08b6
100499541 <100499541@alumnos.uc3m.es>
2026-01-20 00:37:36 +0100

    Initial Unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings")]
+    private Rigidbody2D rb;
+    [SerializeField] private float walkSpeed = 1;
+    private float xAxis;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float jumpForce = 45;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+    Animator anim;
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+        Flip();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+    void Flip()
+    {
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+        else if (xAxis > 0 )
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+    }
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
+        anim.SetBool("Walking", rb.linearVelocity.x != 0 && Grounded());
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)  
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        {
+            return false;
+        }
+    }
+    void Jump()
+    {
+        if(Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        }
+        if(Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+        }
+        anim.SetBool("Jumping", !Grounded());       
+    }
+}
